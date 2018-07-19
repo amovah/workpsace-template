077e35eeb4935ea4d1e4d758e637f40943f45f36
Ali Movahedi <ali_movahedi@aol.com>
2018-07-19 16:06:48 +0430

    feat: new version lb

--- server-lb/bootstrap.sh
+++ server-lb/bootstrap.sh
@@ -1,7 +1,3 @@
 cp -r ./ "$1"
 cd "$1"
-yarn add -D gulp gulp-babel babel-core \
-babel-plugin-add-module-exports babel-plugin-transform-class-properties \
-babel-plugin-transform-decorators-legacy \
-babel-plugin-transform-object-rest-spread babel-preset-env \
-babel-eslint gulp-eslint
+yarn add -D gulp babel-eslint gulp-eslint
--- server-lb/gulpfile.js
+++ server-lb/gulpfile.js
@@ -1,24 +1,11 @@
 /* eslint-disable */
 
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
-
-gulp.task('babel', () =>
-  gulp.src('src/**/*.js')
-  .pipe(babel())
-  .pipe(gulp.dest('./'))
-);
+const lint = require('gulp-eslint');
 
 gulp.task('lint', () =>
-  gulp.src('src/**/*.js')
+  gulp.src('./**/*.js')
   .pipe(lint())
   .pipe(lint.format())
   .pipe(lint.failAfterError())
 );
-
-gulp.task('dev', ['babel'], () => {
-  gulp.watch('src/**/*.js', ['babel']);
-});
-
-gulp.task('prod', ['lint', 'babel']);
