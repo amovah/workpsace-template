072b68e2f7691468c4cd7c8949cfa451fb3649ea
Ali Movahedi <ali_movahedi@aol.com>
2018-09-16 08:00:02 +0430

    feat: update babel version

--- react/bootstrap.sh
+++ react/bootstrap.sh
@@ -1,11 +1,11 @@
 cp -r ./ "$1"
 cd "$1"
-yarn add -D autoprefixer babel-core babel-eslint babel-loader \
-babel-plugin-add-module-exports babel-plugin-transform-class-properties \
-babel-plugin-transform-decorators-legacy \
-babel-plugin-transform-object-rest-spread babel-preset-env babel-preset-react \
+yarn add -D autoprefixer @babel/core babel-eslint babel-loader \
+@babel/preset-env @babel/preset-react \
 css-loader del eslint eslint-plugin-react gulp gulp-eslint gulp-replace \
-less less-loader postcss-loader prop-types react react-dom react-redux \
-react-router-dom redux redux-thunk string-replace-loader style-loader \
+less less-loader postcss-loader string-replace-loader style-loader \
 uglifyjs-webpack-plugin url-loader webpack express gulp-cli prop-types \
 eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y
+
+yarn add prop-types react react-dom react-redux react-router-dom redux \
+redux-thunk
