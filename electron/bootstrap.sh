b194618ffb898c089782309dd79f37114cf20529
Ali Movahedi <ali_movahedi@aol.com>
2018-09-16 08:07:19 +0430

    feat: add electron bootstrap

---
+++ electron/bootstrap.sh
@@ -0,0 +1,9 @@
+cp -r ./ "$1"
+cd "$1"
+yarn add -D autoprefixer @babel/core babel-eslint babel-loader \
+@babel/preset-env @babel/preset-react \
+css-loader del eslint eslint-plugin-react gulp gulp-eslint \
+uglifyjs-webpack-plugin url-loader webpack gulp-cli prop-types \
+eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y
+
+yarn add prop-types react react-dom react-redux react-router-dom redux
