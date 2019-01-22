0780dfb8f4ba013248bd4abb6c2946f79817d8bf
Ali Movahedi <ali_movahedi@aol.com>
2019-01-22 10:08:16 +0330

    feat: add loader for tempalte

--- react/src/config.js
+++ react/src/config.js
@@ -1,6 +1,7 @@
 let config = {
   prodCDN: '',
-  devCDN: `http://localhost:${process.env.NODE_PORT}/static`,
+  devCDN: 'http://localhost:8181/static'
+  port: 8180,
 };
 
 if (process.env.NODE_ENV === 'development') {
