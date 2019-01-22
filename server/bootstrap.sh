8400aaa83ab49961482c076c5c9d88bc33831c23
Ali Movahedi <ali_movahedi@aol.com>
2019-01-22 10:48:15 +0330

    feat: add rundev command

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -2,7 +2,7 @@ cp -r ./ "$1"
 
 cd "$1"
 
-yarn add -D gulp del gulp-babel @babel/core @babel/preset-env \
+yarn add -D gulp del gulp-babel @babel/core @babel/preset-env nodemon \
 babel-eslint gulp-eslint eslint-config-airbnb-base eslint-plugin-import gulp-cli
 
 rm "$1/bootstrap.sh"
